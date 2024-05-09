010a5c070f60c2df389f784a5ca61549ca574a11
Nahuel <Nahuel@DESKTOP-9J56VQ7>
2024-05-09 16:30:23 -0300

    Creacion del Repositorio y Archivos del Proyecto

---
+++ TP03_VilteDaniel/src/main/java/ar/edu/unju/fi/Tp03VilteDanielApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp03VilteDanielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp03VilteDanielApplication.class, args);
+	}
+
+}
